f8623167d41775de372773183dc34c06280bcf70
Angel Pant <angelpant262@gmail.com>
2025-04-11 17:48:39 -0400

    Add files via upload

---
+++ Final Project (HTML)/script.js
@@ -0,0 +1,19 @@
+// Header injection
+document.getElementById('header').innerHTML = `
+  <header>
+    <h2>Sony Walkman</h2>
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="product.html">Product</a>
+      <a href="contact.html">Contact</a>
+    </nav>
+  </header>
+`;
+
+// Footer injection
+document.getElementById('footer').innerHTML = `
+  <footer>
+    <p>&copy; 2025 Sony Corporation. All rights reserved.</p>
+  </footer>
+`;
